9133f05753c6bd1f741b866b9eea97de717a40d1
Philip314 <philip956@hotmail.co.uk>
2018-11-05 17:10:46 +0000

    Assign username to User

--- src/main/user/User.java
+++ src/main/user/User.java
@@ -1,8 +1,21 @@
 package main.user;
 
 public class User {
+	
+	private String username;
 
 	public User() {
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		if (username.equals("")) {
+			throw new IllegalArgumentException("Username cannot be empty");
+		}
+		this.username = username;
 	}
 
 }
--- src/test/UserTest.java
+++ src/test/UserTest.java
@@ -7,4 +7,19 @@ import main.user.User;
 
 public class UserTest {
 	
+	@Test
+	public void setUsernameExceptionNotThrown() {
+		User user = new User();
+		user.setUsername("Test");
+		Assertions.assertEquals("Test", user.getUsername());
+	}
+	
+	@Test
+	public void setEmptyUsernameExceptionThrown() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			User user = new User();
+			user.setUsername("");
+		});
+	}
+	
 }
