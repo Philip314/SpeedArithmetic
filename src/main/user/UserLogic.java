7c490858078654487c32aabc8528df2a9d298ff1
Philip314 <philip956@hotmail.co.uk>
2019-05-11 21:46:34 +0100

    Add JavaDoc to UserLogic

--- src/main/user/UserLogic.java
+++ src/main/user/UserLogic.java
@@ -2,28 +2,53 @@ package main.user;
 
 import main.arithmetictest.ArithmeticTest;
 
-/*
- * This class is for the logic of user class.
+/**
+ * This class handles all the logic of the user class.
+ * 
+ * @author Philip
+ *
  */
-
 public class UserLogic {
 	
+	/**
+	 * The active user of the application.
+	 */
 	private static User activeUser = null;
 	
+	/**
+	 * Gets the current active user.
+	 * 
+	 * @return the active user
+	 */
 	public static User getActiveUser() {
 		return activeUser;
 	}
 	
+	/**
+	 * Sets the active user.
+	 * 
+	 * @param user the user which will become active
+	 */
 	public static void setActiveUser(User user) {
 		activeUser = user;
 	}
 	
+	/**
+	 * Creates a new user.
+	 * 
+	 * @param username the name of the user
+	 */
 	public static void createUser(String username) {
 		User user = new User();
 		user.setUsername(username);
 		setActiveUser(user);
 	}
 	
+	/**
+	 * Saves an ArithmeticTest to the current active user.
+	 * 
+	 * @param test the ArithmeticTest to be saved
+	 */
 	public static void saveTest(ArithmeticTest test) {
 		if (activeUser != null) {
 			activeUser.addTest(test);
